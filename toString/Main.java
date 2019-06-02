186d86759415985e5c10797138541c3721a98300
kimyoungeun <ghkdlxld79@naver.com>
2019-06-02 20:03:08 +0900

    toString

---
+++ toString/Main.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(String[] args) {
+    Person p = new Person("Jackson", 21);
+		System.out.println(p.toString());
+  }
+}
---
+++ toString/Person.java
@@ -0,0 +1,13 @@
+class Person {
+  private String name;
+  private int age;
+  Person(String s_name, int s_age){
+    this.name=s_name;
+    this.age= s_age;
+  }
+  public String toString(){
+    return "My name is " +name +" and I'm "+ age +" years old.";
+  }
+  
+  // Your code here
+}
